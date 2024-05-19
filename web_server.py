ccc2c5488df6e59d45e84b7fd472f2536ee40ccb
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-05-19 11:36:19 -0700

    Added graphs served by mat plot lib

--- web_server.py
+++ web_server.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request
 import sqlite3
 import create_db
+import matplotlib.pyplot as plt
+import numpy as np
 import parse_workout
+
+
+# Use Agg backend for matplotlib
+plt.switch_backend('Agg')
+
 
 # Create a Flask app
 app = Flask(__name__)
 
+
 # Define a function to get the workout data from the database
-
-
 def get_workout_data():
     conn = sqlite3.connect('workouts_data.db')
     c = conn.cursor()
@@ -68,10 +74,28 @@ def delete_workout(workout_id):
     return "Workout deleted successfully"
 
 
-# Let user visualize the data with graphs
+# Define a route for the visualize URL
 @app.route('/visualize')
 def visualize():
-    return render_template('visualize.html')
+    workouts = get_workout_data()
+    for workout_id, exercises in workouts.items():
+        exercise_weights = {}
+        for exercise in exercises:
+            exercise_name, set_number, weight, reps = exercise
+            if exercise_name not in exercise_weights:
+                exercise_weights[exercise_name] = []
+            exercise_weights[exercise_name].append(weight)
+
+        x = np.arange(len(exercise_weights))
+        weights = [np.sum(weights) for weights in exercise_weights.values()]
+        plt.bar(x, weights)
+        plt.xticks(x, exercise_weights.keys())
+        plt.xlabel('Exercise')
+        plt.ylabel('Total Weight Lifted')
+        plt.title('Weight Lifted per Exercise')
+        plt.savefig('static/workout_{}.png'.format(workout_id[2]))
+        plt.clf()
+    return render_template('visualize.html', workouts=workouts)
 
 
 # Run the app
